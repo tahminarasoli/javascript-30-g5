a8ab7d48bf9fe7aee956c74a4f38d33dce524d48
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2020-12-19 11:52:11 +0100

    where is it used?

--- Custom-Video-Player/src/data.js
+++ Custom-Video-Player/src/data.js
@@ -6,6 +6,8 @@
   the "better" way to do this would be to set a .mousedown property in an exported object
   then all files can read/write this property and by side-effect will always have the up-to-date data
   using an object to store all your program's data is the objective in the next module, so you're just on time
+  
+  (i also can't find anywhere in your project that this variable is read, only set)
 */
 
 /**
