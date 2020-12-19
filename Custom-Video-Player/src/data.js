711b9dcae643a0a99b1894704a18a5a8788067fd
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2020-12-19 11:48:07 +0100

    suggest: export object with mousedown property

--- Custom-Video-Player/src/data.js
+++ Custom-Video-Player/src/data.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/*
+  it's a clever idea to create a setter function, but then this file is no longer just data
+  
+  the "better" way to do this would be to set a .mousedown property in an exported object
+  then all files can read/write this property and by side-effect will always have the up-to-date data
+  using an object to store all your program's data is the objective in the next module, so you're just on time
+*/
+
 /**
  * 
  * @type {boolean}
